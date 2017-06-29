7adf0c762b1cce3b075362d5cb9b4787c0404d07
Francois Roul <francois.roul@orange.com>
2017-06-29 14:37:45 +0200

    init new repo

---
+++ src/main/java/yose/Yose.java
@@ -0,0 +1,46 @@
+package yose;
+
+import com.google.gson.Gson;
+import com.vtence.molecule.WebServer;
+import com.vtence.molecule.routing.DynamicRoutes;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Yose {
+
+    private final WebServer server;
+
+    public Yose(int port) {
+        this.server = WebServer.create(port);
+    }
+
+    public void start() throws IOException {
+        final Gson gson = new Gson();
+
+        server.start(new DynamicRoutes() {{
+            get("/").to((request, response) -> response.body("Hello Yose"));
+            get("/ping").to(new Ping(gson)::pong);
+        }});
+    }
+
+    public URI uri() {
+        return server.uri();
+    }
+
+    public void stop() throws IOException {
+        server.stop();
+    }
+
+    private static final int PORT = 0;
+
+    private static int port(String[] args) {
+        return args.length > 0 ? Integer.parseInt(args[PORT]) : 8080;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Yose yose = new Yose(port(args));
+        yose.start();
+        System.out.print("To play the game visit " + yose.uri());
+    }
+}
---
+++ src/test/java/yose/worlds/StartWorld.java
@@ -0,0 +1,44 @@
+package yose.worlds;
+
+import com.vtence.molecule.testing.http.HttpRequest;
+import com.vtence.molecule.testing.http.HttpResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import yose.YoseDriver;
+
+import java.io.IOException;
+
+import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
+
+public class StartWorld {
+
+    YoseDriver yose = new YoseDriver(9999);
+
+    HttpRequest request = new HttpRequest(9999);
+    HttpResponse response;
+
+    @Before
+    public void startGame() throws Exception {
+        yose.start();
+    }
+
+    @After
+    public void stopGame() throws Exception {
+        yose.stop();
+    }
+
+    @Test
+    public void firstWebPageChallenge() throws IOException {
+        //yose.home().displaysGreeting("Hello Yose");
+    }
+
+    @Test
+    public void firstWebServiceChallenge() throws IOException {
+        response = request.get("/ping");
+
+        assertThat(response).isOK()
+                            .hasContentType("application/json")
+                            .hasBodyText("{\"alive\":true}");
+    }
+}
